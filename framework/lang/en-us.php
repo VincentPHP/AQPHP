5b7bb9859a99ed15b22cd6ee71f86fb5dc3eee8b
Vincent <admin@gzibm.com>
2018-06-03 12:34:42 +0800

    Delete Develop Code

---
+++ framework/lang/en-us.php
@@ -0,0 +1,15 @@
+<?php
+/* +----------------------------------------------------------------
+ * | Software: [AQPHP framework]
+ * |  WebSite: www.aqphp.com
+ * |----------------------------------------------------------------
+ * | Author: 赵 港 <Vincent> < admin@gzibm.com | 847623251@qq.com >
+ * | WeChat: GZIDCW
+ * | Copyright (C) 2015-2020, www.aqphp.com All Rights Reserved.
+ * +----------------------------------------------------------------*/
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/1/10
+ * Time: 16:01
+ */
--- public/static/mancen/js/kitconfig.js
+++
@@ -1,8 +0,0 @@
-layui.define(function(exports) {
-
-    var kitconfig = {
-        resourcePath: 'static/mancen/', //框架资源路径-相对路径和绝对路径
-    };
-
-    exports('kitconfig', kitconfig);
-});
